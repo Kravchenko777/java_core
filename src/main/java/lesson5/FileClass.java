d85932d590fa977db17cb94cb10f69a7af75d127
Install <kravmaxim@gmail.com>
2022-02-23 12:59:20 +0400

    lesson5

--- src/main/java/lesson5/FileClass.java
+++ src/main/java/lesson5/FileClass.java
@@ -1,4 +1,27 @@
 package lesson5;
 
+import java.io.File;
+
 public class FileClass {
+
+    public static void main(String[] args) {
+        File fileCSV = new File("src/main/resources/test.csv");
+
+        System.out.println(fileCSV.getName());
+        System.out.println(fileCSV.exists());
+        System.out.println(fileCSV.isDirectory());
+
+
+        File directoryCSV = new File("src/main/resources");
+
+        System.out.println(directoryCSV.getName());
+        System.out.println(directoryCSV.exists());
+        System.out.println(directoryCSV.isDirectory());
+
+        for (String str: directoryCSV.list()){
+            System.out.println("file name: " + str);
+        }
+
+
+    }
 }
